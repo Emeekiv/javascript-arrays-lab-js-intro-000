cc1cd5046fb0512373036e45c2a59c59ccf515aa
Edwin Meek <emeekiv@gmail.com>
2019-12-14 19:22:59 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,3 +17,19 @@ function destructivelyRemoveFirstKitten(name){
   kittens.shift(name)
   return kittens
 }
+function appendKitten(name){
+var kittens2 = kittens.splice(kittens.length, 0, name)
+  return kittens2
+}
+function prependKitten(name){
+var kittens2 = kittens.splice(0, 0, name)
+  return kittens2
+}
+function removeLastKitten(name){
+var kittens2 = kittens.slice(-1)
+  return kittens2
+}
+function removeFirstKitten(name){
+var kittens2 = kittens.slice(1)
+  return kittens2
+}
